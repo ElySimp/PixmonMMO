e160c8a8f9ccd367ba8c3e730b852b12bf5e4554
LuckRaf <raf.austin05@gmail.com>
2025-06-14 19:15:30 +0700

    db table restructure with JSON

--- backend/models/Inventory.js
+++ backend/models/Inventory.js
@@ -8,10 +8,8 @@ class Inventory {
                 item_name VARCHAR(100) DEFAULT NULL,
                 description VARCHAR(100) DEFAULT NULL,
                 item_type VARCHAR(100) DEFAULT NULL,
-                max_base INT(11) DEFAULT NULL,
-                min_base INT(11) DEFAULT NULL,
                 rarity INT(11) DEFAULT NULL,
-                effect_base INT(11) DEFAULT NULL
+                item_Stats JSON DEFAULT NULL
              )
         `;
           try {
@@ -33,13 +31,12 @@ class Inventory {
         const sql = `
             CREATE TABLE IF NOT EXISTS UserInventory (
                 item_name varchar(100),
-                atk_value int,
-                effect_value int, 
-                def_value int,
                 index_id int(11),
                 user_id int(11),
                 item_type varchar(100),
+                item_stats JSON DEFAULT NULL,
                 amount int,
+                rarity int(11) DEFAULT NULL,
                 FOREIGN KEY (index_id) REFERENCES IndexInventory (index_id) ON DELETE CASCADE,
                 FOREIGN KEY (user_id) REFERENCES UserLogin(id) ON DELETE CASCADE
             );
@@ -58,11 +55,9 @@ class Inventory {
         const sql = `
             CREATE TABLE IF NOT EXISTS gachaResult (
                 item_name varchar(100),
-                atk_value int(11), 
-                effect_value int(11),
-                def_value int(11),
                 index_id int(11), 
                 user_id int(11), 
+                item_stats JSON DEFAULT NULL,
                 item_type varchar(100), 
                 created_at DATETIME,
                 FOREIGN KEY (user_id) REFERENCES UserLogin(id) ON DELETE CASCADE
@@ -83,100 +78,103 @@ class Inventory {
                 item_name:"test item no 1", 
                 description:"idk item",
                 item_type:"consumables",
-                max_base: 20,
-                min_base: 10,
+                item_stats: {
+                    atk_value: 0,
+                    effect_value: 0, 
+                    def_value: 0
+                },
                 rarity: 2, 
-                effect_base: null
             },
             {
                 item_id: 2,
                 item_name:"Mana Potion", 
                 description:"A potion that somehow recovers mana",
                 item_type:"potion",
-                max_base: 0,
-                min_base: 0,
+                item_stats: {
+                    mana_regen: 30, 
+                },
                 rarity: 3, 
-                effect_base: 30
             },
             {
                 item_id: 3,
                 item_name:"Health Potion", 
                 description:"A potion that somehow recovers health",
                 item_type:"potion",
-                max_base:0,
-                min_base:0,
-                rarity:2, 
-                effect_base: 30
+
+                item_stats: {
+                    health_regen: 30
+                },
+                rarity:2 
             },
             {
                 item_id: 4,
                 item_name:"Snaccident", 
                 description:"Yall accidentally made this",
                 item_type:"food",
-                max_base:0,
-                min_base:0,
-                rarity:2, 
-                effect_base:40
+                item_stats: {
+                    hunger_value: 40
+                },
+                rarity:2 
             },
             {
                 item_id: 5,
                 item_name:"Marshmelt", 
                 description:"A melted marshmallow cool right",
                 item_type:"food",
-                max_base:0,
-                min_base:0,
-                rarity:3, 
-                effect_base:25
+                item_stats: {
+                    hunger_value: 25
+                },
+                rarity:3 
             },
             {
                 item_id: 6,
                 item_name:"Cheese", 
                 description:"Say cheese",
                 item_type:"food",
-                max_base:0,
-                min_base:0,
-                rarity:2, 
-                effect_base:20
+                item_stats: {
+                    hunger_value: 20
+                },
+                rarity:2 
             },
             {
                 item_id: 7,
                 item_name:"Chonklette", 
                 description:"Chonklette’s diet? All the snacks!",
                 item_type:"food",
-                max_base:0,
-                min_base:0,
-                rarity:3, 
-                effect_base:35
+                item_stats: {
+                    hunger_value: 35
+                },
+                rarity:3 
             },
             {
                 item_id: 8,
                 item_name:"Sizzlet", 
                 description:"Just a sizzled food what do you expect",
                 item_type:"food",
-                max_base:0,
-                min_base:0,
-                rarity:1, 
-                effect_base:30
+                item_stats: {
+                    hunger_value: 30
+                },
+                rarity:1 
             },
             {
                 item_id: 9,
                 item_name:"Steel Key", 
                 description:"A key to open up a metal chest",
                 item_type:"key",
-                max_base:0,
-                min_base:0,
-                rarity:2, 
-                effect_base:0
+                item_stats: {
+                    key_level: 1
+                },
+                rarity:2
             },
             {
                 item_id: 10,
                 item_name:"Mythical Key", 
                 description:"A key to open up a mythical chest",
                 item_type:"key",
-                max_base:0,
-                min_base:0,
+                item_stats: {
+                    key_level: 2
+                },
                 rarity:4, 
-                effect_base:0
             }
 
         ];
@@ -187,56 +185,22 @@ class Inventory {
             try {
                 await db.query(`
                     INSERT INTO IndexInventory 
-                    (index_id, item_name, description, item_type, max_base, min_base, rarity, effect_base) 
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                    (index_id, item_name, description, item_type, item_stats, rarity) 
+                    VALUES (?, ?, ?, ?, ?, ?)
                 `, [
                     item.item_id, 
                     item.item_name,
                     item.description,
                     item.item_type,
-                    item.max_base,
-                    item.min_base, 
-                    item.rarity, 
-                    item.effect_base
+                    JSON.stringify(item.item_stats),
+                    item.rarity
                 ]);
             } catch (error) {
                 console.error(error);
             }
         }
         
-        console.log('Seeded initial items');
-    }
-
-
-    static async force_send (id) {
-        const test_items = [
-            {
-                item_name: "test item no 1",
-                atk_value: 0,
-                effect_value: 20, 
-                def_value: 0,
-                index_id: 1 ,
-                user_id: 1
-            }
-        ]
-
-        try {            await db.query(`
-                    INSERT INTO UserInventory 
-                    (item_name, atk_value, effect_value, def_value, index_id, user_id) 
-                    VALUES (? , ? , ? , ? , ? , ?)
-                `, [
-                    test_items.item_name,
-                    test_items.atk_value,
-                    test_items.effect_value, 
-                    test_items.def_value,
-                    test_items.index_id,
-                    test_items.user_id
-                ]);
-            // Test item force sent to UserInventory
-        } catch (error){
-            console.error('Error sending data to UserInventory table:', error);
-            throw error;
-        }
+        
     }
 
     static async countUserInventory(userId) {
@@ -308,15 +272,13 @@ class Inventory {
                 // gacha history
                 await db.query(
                     `INSERT INTO gachaResult 
-                        (item_name, atk_value, effect_value, def_value, index_id, user_id, item_type, created_at)
+                        (item_name, index_id, user_id, item_type, item_stats, created_at)
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
                     [
                         selectedItem.item_name,
-                        0, // atk_value placeholder
-                        selectedItem.effect_base || 0,
-                        0, // def_value placeholder
                         selectedItem.index_id,
                         userId,
+                        selectedItem.item_stats ? JSON.stringify(selectedItem.item_stats) : null,
                         selectedItem.item_type, 
                         now
                     ]
@@ -330,18 +292,17 @@ class Inventory {
                 if (rows[0].count === 0) {
                     await db.query(`
                         INSERT INTO UserInventory 
-                            (item_name, atk_value, effect_value, def_value, index_id, user_id, item_type, amount)
+                            (item_name, index_id, user_id, item_type, amount, item_stats, rarity)
                             VALUES (?, ?, ?, ?, ?, ?, ?, ?)
                         `, 
                         [
                             selectedItem.item_name, 
-                            0, // atk
-                            selectedItem.effect_base,
-                            0, // def
                             selectedItem.index_id,
                             userId, 
                             selectedItem.item_type, 
-                            1
+                            1,
+                            selectedItem.item_stats ? JSON.stringify(selectedItem.item_stats) : null,
+                            selectedItem.rarity
                         ]
                     );
                 }
